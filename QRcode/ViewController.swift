73c195ead87031f546db30963fcb058edd8ea403
リボルバー・オセロット <revolverocelot@net71-dhcp-190.sfc.keio.ac.jp>
2016-08-03 15:54:43 +0900

    Initial Commit

---
+++ QRcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRcode
+//
+//  Created by リボルバー　オセロット on 2016/08/03.
+//  Copyright © 2016年 ocelot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
